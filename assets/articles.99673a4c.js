e50dc4e55553ddd39e8e532dd894463dea785ff8
孙华鹏 <sunhuapeng@heydayscape.com>
2021-08-27 13:49:19 +0800

    更新3.0

---
+++ assets/articles.99673a4c.js
@@ -0,0 +1,1 @@
+import{g as t}from"./index.391a9efd.js";const e={state:{articles:[]},getters:{getArticles:t=>t.articles,getTags:t=>{let e=[];return t.articles.forEach((t=>{e.push(...t.tags),e=[...new Set(e)]})),e}},actions:{async setArticles({commit:e,state:s}){e("articles",[...await t()])}},mutations:{articles(t,e){t.articles=e}}};export{e as articles};
