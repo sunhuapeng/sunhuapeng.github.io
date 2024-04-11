77700a551acb36f5e798a9d6961843f3fc883160
绛河 <jianghe@uni-ubi.com>
2024-04-11 15:27:59 +0800

    商场

--- code/src/components/createCss3d.ts
+++
@@ -1,9 +0,0 @@
-import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer";
-
-export const createCss3d = (html: string, classname?: string): CSS3DObject => {
-    let div = document.createElement('div');
-    div.innerHTML = html;
-    classname && div.classList.add(classname)
-    let object = new CSS3DObject(div);
-    return object
-}
